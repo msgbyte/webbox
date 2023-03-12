d24416f66a19d2bcfce536ab8763c2a2774adb59
moonrailgun <moonrailgun@gmail.com>
2023-03-12 17:04:06 +0800

    style: remove unused import

--- src/renderer/utils/hooks/useEditValue.ts
+++ src/renderer/utils/hooks/useEditValue.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
 
 export function useEditValue<T>(value: T, onChange: (val: T) => void) {
   const [inner, setInner] = useState(value);
